470945170e60192870875dadb491892a40026a11
Daniel Slapelis <daniel@daniels-imac.frontierlocal.net>
2017-03-13 12:10:25 -0400

    Initial Commit

---
+++ Out With Friends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Out With Friends
+//
+//  Created by Daniel Slapelis on 3/13/17.
+//  Copyright © 2017 Dream Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
